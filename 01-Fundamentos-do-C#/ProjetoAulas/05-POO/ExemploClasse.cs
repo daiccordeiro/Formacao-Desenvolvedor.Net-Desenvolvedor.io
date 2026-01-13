ac704651bf7120c7ebc128efa3803d417a9e5fcd
Daiane Cordeiro <redesdaiane@gmail.com>
2026-01-13 10:32:30 -0300

    POO - Exemplo de Classe

---
+++ 01-Fundamentos-do-C#/ProjetoAulas/05-POO/ExemploClasse.cs
@@ -0,0 +1,43 @@
+//namespace Cadastro;
+
+namespace Cadastro
+{
+    public static class Calculos
+    {
+        public static int SomarNumeros(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Produto
+    {
+        private int Id;
+
+        public string? Descricao { get; set;}
+
+        // somente leitura
+        public readonly int Estoque;
+        //public int Estoque { get; }
+
+        public Produto()
+        {
+            Estoque = 1;
+        }
+
+        public void ImprimirDescricao()
+        {
+            Console.WriteLine(GetId() + " - " + Descricao);
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+    }
+}
--- 01-Fundamentos-do-C#/ProjetoAulas/Program.cs
+++ 01-Fundamentos-do-C#/ProjetoAulas/Program.cs
@@ -1,90 +1,131 @@
 ﻿using System;
 
-Console.WriteLine("Meu Primeiro Programa!");
+namespace Application
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu Primeiro Programa!");
 
-//01-HandsOnCode
-//Variaveis.cs
-Console.WriteLine("Exemplo de variáveis:");
-Variaveis.ExibirVariaveis();
+            //01-HandsOnCode
+            //Variaveis.cs
+            Console.WriteLine("Exemplo de variáveis:");
+            Variaveis.ExibirVariaveis();
 
-//Constantes.cs
-Console.WriteLine("Exemplo de constantes:");
-Constantes.ExibirConstantes();
+            //Constantes.cs
+            Console.WriteLine("Exemplo de constantes:");
+            Constantes.ExibirConstantes();
 
-//OpAritmeticos.cs
-Console.WriteLine("Exemplo de operadores aritméticos:");
-OpAritmeticos.ExibirOpAritmeticos();
+            //OpAritmeticos.cs
+            Console.WriteLine("Exemplo de operadores aritméticos:");
+            OpAritmeticos.ExibirOpAritmeticos();
 
-//OpRelacionais.cs
-Console.WriteLine("Exemplo de operadores relacionais:");
-OpRelacionais.ExibirOpRelacionais();
+            //OpRelacionais.cs
+            Console.WriteLine("Exemplo de operadores relacionais:");
+            OpRelacionais.ExibirOpRelacionais();
 
-//OpLogicos.cs
-Console.WriteLine("Exemplo de operadores lógicos:");
-OpLogicos.ExibirOpLogicos();
+            //OpLogicos.cs
+            Console.WriteLine("Exemplo de operadores lógicos:");
+            OpLogicos.ExibirOpLogicos();
 
-//OpTernario.cs
-Console.WriteLine("Exemplo de operador ternário:");
-OpTernario.ExibirOpTernario();
-
-
-//02-Funções
-//funNomeCompleto.cs
-Console.WriteLine("Exemplo de Função:");
-FunNomeCompleto.ExibirFuncaoNomeCompleto();
-
-//funParametros.cs
-Console.WriteLine("Exemplo de Função com Parâmetros:");
-FunParametros.ExibirFuncaoParametros();
+            //OpTernario.cs
+            Console.WriteLine("Exemplo de operador ternário:");
+            OpTernario.ExibirOpTernario();
 
 
-//03-Estrutura de Dados
-//ArrayList.cs
-Console.WriteLine("Exemplo de ArrayList:");
-ExemploArrayList.ExibirArrayList();
+            //02-Funções
+            //funNomeCompleto.cs
+            Console.WriteLine("Exemplo de Função:");
+            FunNomeCompleto.ExibirFuncaoNomeCompleto();
 
-//ArrayTipado.cs
-Console.WriteLine("Exemplo de ArrayTipado:");
-ExemploArrayTipado.ExibirArrayTipado();
-
-//Lista.cs
-Console.WriteLine("Exemplo de Lista:");
-ExemploLista.ExibirLista();
-
-//Dicionario.cs
-Console.WriteLine("Exemplo de Dicionário:");
-ExemploDicionario.ExibirDicionario();
-
-//Fila.cs
-Console.WriteLine("Exemplo de Fila:");
-ExemploFila.ExibirFila();
-
-//Pilha.cs
-Console.WriteLine("Exemplo de Pilha:");
-ExemploPilha.ExibirPilha();
+            //funParametros.cs
+            Console.WriteLine("Exemplo de Função com Parâmetros:");
+            FunParametros.ExibirFuncaoParametros();
 
 
-//04-Estrutura de Controle
-//ExemploIFeElse.cs
-Console.WriteLine("Exemplo de IF e Else:");
-ExemploIFeElse.ExibirIFeElse();
+            //03-Estrutura de Dados
+            //ArrayList.cs
+            Console.WriteLine("Exemplo de ArrayList:");
+            ExemploArrayList.ExibirArrayList();
 
-//ExemploSwitch.cs
-Console.WriteLine("Exemplo de Switch:");
-ExemploSwitch.ExibirSwitch();
+            //ArrayTipado.cs
+            Console.WriteLine("Exemplo de ArrayTipado:");
+            ExemploArrayTipado.ExibirArrayTipado();
 
-//ExemploFor.cs
-Console.WriteLine("Exemplo de FOR:");
-ExemploFor.ExibirFor();
+            //Lista.cs
+            Console.WriteLine("Exemplo de Lista:");
+            ExemploLista.ExibirLista();
 
-//ExemploForEach.cs
-Console.WriteLine("Exemplo de ForEach:");
-ExemploForEach.ExibirForEach();
+            //Dicionario.cs
+            Console.WriteLine("Exemplo de Dicionário:");
+            ExemploDicionario.ExibirDicionario();
 
-//ExemploWhile.cs
-Console.WriteLine("Exemplo de While:");
-ExemploWhile.ExibirWhile();
+            //Fila.cs
+            Console.WriteLine("Exemplo de Fila:");
+            ExemploFila.ExibirFila();
 
-//ExemploBreakeContinue.cs
-Console.WriteLine("Exemplo de Break e Continue:");
-ExemploBreakeContinue.ExibirBreakeContinue();
+            //Pilha.cs
+            Console.WriteLine("Exemplo de Pilha:");
+            ExemploPilha.ExibirPilha();
+
+
+            //04-Estrutura de Controle
+            //ExemploIFeElse.cs
+            Console.WriteLine("Exemplo de IF e Else:");
+            ExemploIFeElse.ExibirIFeElse();
+
+            //ExemploSwitch.cs
+            Console.WriteLine("Exemplo de Switch:");
+            ExemploSwitch.ExibirSwitch();
+
+            //ExemploFor.cs
+            Console.WriteLine("Exemplo de FOR:");
+            ExemploFor.ExibirFor();
+
+            //ExemploForEach.cs
+            Console.WriteLine("Exemplo de ForEach:");
+            ExemploForEach.ExibirForEach();
+
+            //ExemploWhile.cs
+            Console.WriteLine("Exemplo de While:");
+            ExemploWhile.ExibirWhile();
+
+            //ExemploBreakeContinue.cs
+            Console.WriteLine("Exemplo de Break e Continue:");
+            ExemploBreakeContinue.ExibirBreakeContinue();
+
+
+            //05-POO - Programação Orientada a Objetos
+            //ExemploClasse.cs
+            Console.WriteLine("Exemplo de Classes:");
+            AulaClasses();
+            AulaPropSomenteLeitura();
+        }
+
+        private static void AulaClasses()
+        {
+            var resultado = Cadastro.Calculos.SomarNumeros(2,3);
+            Console.WriteLine(resultado);
+            
+            var produto = new Cadastro.Produto();
+            produto.SetId(1);   
+            
+
+            produto.Descricao = "Teclado";
+            produto.ImprimirDescricao();
+            //Console.WriteLine(produto.GetId());            
+        }
+
+        private static void AulaPropSomenteLeitura()
+        {
+            var produto = new Cadastro.Produto();
+            //produto.Estoque = 1;
+            Console.WriteLine(produto.Estoque);
+            
+
+            produto.Descricao = "Mouse";            
+            produto.ImprimirDescricao();                        
+        }
+    }
+}
