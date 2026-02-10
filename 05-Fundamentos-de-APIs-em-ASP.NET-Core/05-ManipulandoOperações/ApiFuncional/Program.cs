e30cdc739ded345f90ed7f8c6fbc28e201f68e66
Daiane Cordeiro <redesdaiane@gmail.com>
2026-02-09 22:00:01 -0300

    Definindo a Entidade

---
+++ 05-Fundamentos-de-APIs-em-ASP.NET-Core/05-ManipulandoOperações/ApiFuncional/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Acesse /swagger para mais informações");
+app.MapGet("/teste-get", () => "Teste");
+
+app.Run();
